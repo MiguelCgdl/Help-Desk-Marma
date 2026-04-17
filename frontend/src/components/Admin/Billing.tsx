b9b675bd1d413c2365511cbf2f3001e3d3004e22
JonhC <jmiguelcervantesb@gmail.com>
2026-04-16 20:58:09 -0600

    fix: remove erroneous character from Billing component import statement

--- frontend/src/components/Admin/Billing.tsx
+++ frontend/src/components/Admin/Billing.tsx
@@ -1,4 +1,4 @@
-1import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 import type { Company, Ticket } from '../../types';
 import {
